e5923190865bb2db1e90cae2f9db00f121703978
TheoDerive <theo.derive.pro@gmail.com>
2025-05-01 23:38:19 +0200

    Fix: build project

--- dist/lightManager.d.ts
+++ dist/lightManager.d.ts
@@ -2,6 +2,7 @@ import * as THREE from "three";
 export declare class LightManager {
     static maxLights: number;
     private static lightData;
+    private static lightDebug: { index: number, mesh: THREE.Mesh }[];
     static lightCount: number;
     private static lightTexture;
     private static pixelRatio;
@@ -25,6 +26,7 @@ export declare class LightManager {
      *
      */
     static updateLight(index: number, position: [number, number, number], intensity: number): void;
+    private static updateDebugLight;
     static getPixelRatio(): number;
     /**
      * Upadate light pixelRatio
@@ -33,4 +35,10 @@ export declare class LightManager {
      *
      */
     static setPixelRatio(value: number): void;
+    /**
+     * Debug light
+     *
+     * @param { THREE.Scene } scene - The scene where you want the helper be render
+     */
+    static debugLight(scene: THREE.Scene): void;
 }
--- dist/lightManager.js
+++ dist/lightManager.js
@@ -2,6 +2,7 @@ import * as THREE from "three";
 export class LightManager {
   static maxLights = 100;
   static lightData = new Float32Array(LightManager.maxLights * 4);
+  static lightDebug = [];
   static lightCount = 0;
   static lightTexture = new THREE.DataTexture(
     LightManager.lightData,
@@ -54,6 +55,20 @@ export class LightManager {
     this.lightData[base + 3] = intensity;
     this.lightTexture.needsUpdate = true;
     this.updateUniforms();
+
+    if (this.lightDebug.length > 0) {
+      this.updateDebugLight(
+        index,
+        new THREE.Vector3(position[0], position[1], position[2]),
+      );
+    }
+  }
+  static updateDebugLight(index, newPosition) {
+    const light = this.lightDebug.find((l) => l.index === index);
+
+    if (!light) return;
+
+    light.mesh.position.copy(newPosition);
   }
   static getPixelRatio() {
     return this.pixelRatio;
@@ -61,5 +76,28 @@ export class LightManager {
   static setPixelRatio(value) {
     this.pixelRatio = value;
     this.updateUniforms();
+  }
+  static debugLight(scene) {
+    for (let i = 0; i < this.lightCount; i++) {
+      const position = {
+        x: this.lightData[i],
+        y: this.lightData[i + 1],
+        z: this.lightData[i + 2],
+      };
+
+      const lightHelper = new THREE.Mesh(
+        new THREE.SphereGeometry(0.2, 16, 16),
+        new THREE.MeshBasicMaterial(),
+      );
+
+      lightHelper.position.set(position.x, position.y, position.z);
+
+      scene.add(lightHelper);
+
+      this.lightDebug.push({
+        index: i,
+        mesh: lightHelper,
+      });
+    }
   }
 }
